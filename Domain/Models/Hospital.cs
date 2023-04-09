103f774403fd90b8be53c3fbfc282d18a12e4faf
maqsudj0n1 <maqsudhaydarov1203@gmail.com>
2023-04-09 14:01:06 +0500

    deleted odd files

--- Domain/Models/Hospital.cs
+++ Domain/Models/Hospital.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Domain.States;
 namespace Domain.Models
 {
-<<<<<<<< HEAD:Domain/Models/Hospital.cs
     public class Hospital
     {
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string Address { get; set; }
 
-========
-    public class HospitalPhisician
-    {
-        public int Id { get; set; }
-        public required Hospital Hospital { get; set; }
-        public required Physician Physician { get; set; }
->>>>>>>> b46cbb04968a89239bb83e3e4f517e70d0c4b3b0:Domain/Models/HospitalPhisician.cs
+
+        public class HospitalPhisician
+        {
+            public int Id { get; set; }
+            public required Hospital Hospital { get; set; }
+            public required Physician Physician { get; set; }
+
+            public class Hospital
+            {
+                public int HospitalId { get; set; }
+                public string HospitalName { get; set; }
+                public string Address { get; set; }
+                public Rating Rating { get; set; }
+
+            }
+        }
     }
 }
