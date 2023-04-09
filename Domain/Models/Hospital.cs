bf07d5d23e869e5f625c3b8cb967c22baf113413
maqsudj0n1 <maqsudhaydarov1203@gmail.com>
2023-04-09 14:43:14 +0500

    add new files

--- Domain/Models/Hospital.cs
+++ Domain/Models/Hospital.cs
@@ -7,21 +7,5 @@ namespace Domain.Models
         public required string Name { get; set; }
         public required string Address { get; set; }
 
-
-        public class HospitalPhisician
-        {
-            public int Id { get; set; }
-            public required Hospital Hospital { get; set; }
-            public required Physician Physician { get; set; }
-
-            public class Hospital
-            {
-                public int HospitalId { get; set; }
-                public string HospitalName { get; set; }
-                public string Address { get; set; }
-                public Rating Rating { get; set; }
-
-            }
-        }
     }
 }
