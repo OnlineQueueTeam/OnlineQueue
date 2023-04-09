40cb3c389242581e552ff57735a0457faada8b61
maqsudj0n1 <maqsudhaydarov1203@gmail.com>
2023-04-09 14:47:18 +0500

    committed

--- Domain/Models/HospitalPhisician.cs
+++ Domain/Models/HospitalPhisician.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Domain.Models
 {
+<<<<<<<< HEAD:Domain/Models/Hospital.cs
+    public class Hospital
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+
+========
     public class HospitalPhisician
     {
         public int Id { get; set; }
         public required Hospital Hospital { get; set; }
         public required Physician Physician { get; set; }
+>>>>>>>> b46cbb04968a89239bb83e3e4f517e70d0c4b3b0:Domain/Models/HospitalPhisician.cs
     }
 }
